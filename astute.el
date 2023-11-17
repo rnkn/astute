0820d40352eef4809342f604f4d0b9f8787d393e
Paul W. Rankin <rnkn@rnkn.xyz>
2023-11-17 18:04:52 +1000

    Add README/Commentary

--- astute.el
+++ astute.el
@@ -20,7 +20,21 @@
 
 ;;; Commentary:
 
-;;
+;; Astute
+;; ======
+
+;; A simple Emacs minor mode to redisplay ASCII punctuation as typographic
+;; (or "smart") punctuation without altering file contents.
+
+;; The following characters are redisplayed:
+
+;; - '' -> ‘’ (single quotes)
+;; - "" -> “” (double quotes)
+;; - -- -> – (en dash)
+;; - --- -> — (em dash)
+
+;; Left/right quote is based on word boundary, not correct grammar. This is
+;; intentional.
 
 ;;; Code:
 
